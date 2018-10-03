417a5b3822ea1e12abae8b43fecd9ede8f72e0b9
SasiniSithara <32619284+SasiniSithara@users.noreply.github.com>
2018-10-03 10:56:33 +0530

    Update HelloWorld.java

--- java/HelloWorld.java
+++ java/HelloWorld.java
@@ -4,5 +4,6 @@ import java.io.*;
 class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Hello World and Happy Day");
     }
 }
